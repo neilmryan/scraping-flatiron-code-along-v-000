9b175b95bc562bc724b638d554636854db6cae94
Neil Ryan <neilmarkoliverryan@gmail.com>
2019-11-24 01:40:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,6 +10,7 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
     doc.css(".post").each do |post|
+      course = Course.new
     end
   end
   
